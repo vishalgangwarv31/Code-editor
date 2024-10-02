79287039972be5ec638645fce8e1ca14dfa00f41
Vishal <vishalgangwar8696@gmail.com>
2024-10-02 17:31:47 +0530

    first commit

--- vite-project/src/components/CodeEditor.tsx
+++ vite-project/src/components/CodeEditor.tsx
@@ -4,13 +4,15 @@ import MonacoEditor from "@monaco-editor/react";
 interface CodeEditorProps {
   language: string;
   value: string;
-  onChange: (value: string | undefined) => void;
+  onChange: (value: string) => void; // Ensure `value` is always `string`
 }
+
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ language, value, onChange }) => {
   const handleEditorChange = (newValue: string | undefined) => {
-    onChange(newValue);
+    onChange(newValue ?? '');
   };
+  
 
   return (
     <MonacoEditor
